98f482e465fe539a603990eda0ef058df02d9e8c
Dawchih Liou <dawochih.liou@gmail.com>
2017-03-31 08:20:16 +0000

    setup mongodb

--- server.js
+++ server.js
@@ -2,10 +2,15 @@
 
 const express = require('express');
 const url = require('url');
+const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 const port = process.env.PORT || 8080;
+const dbUrl = process.env.MLAB_URI;
 
+/**
+ * Landing page 
+ */
 app.get('/', function(req, res) {
     res.send('hi');
 });
@@ -16,23 +21,36 @@ app.get('/', function(req, res) {
 app.get(/api\/short\/.*[\:\/]*.*/, function(req, res) {
     const urlRegex = /(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
     const urlMatch = url.parse(req.url).pathname.match(urlRegex);
-    
-    // handling invalid url
-    if (!urlMatch)
-        res.send({error: 'invalid url'});
-    
     const result = {};
     const originalUrl = urlMatch[0];
+    const urlCode = new Date().getTime();
 
+    // handle invalid url
+    if (!urlMatch) return res.send({error: 'Invalid url'});
+    
     result.original_url = originalUrl;
-    result.short_url = req.headers.host + '/' + new Date().getTime();
+    result.short_url = `${req.headers.host}/${urlCode}`;
     
-    // TODO: save in database
-    
-    res.send(result);
-    
+    // save in database
+    MongoClient.connect(dbUrl, (err, db) => {
+        if (err) return console.error(`Unable to connect to mongoDB. ${err}`);
+        
+        console.log(`Connection established to ${dbUrl}`);
+        
+        db.collection('url').save({[urlCode]: originalUrl}, (err, status) => {
+            if (err) return console.error(`Uable to write to mongoDB. ${err}`);
+            
+            db.close();
+            console.log(`Saved to database.`);
+            
+            res.send(result);
+        });
+    });
 });
 
+/**
+ *  Redirect short url to the real site
+ */
 app.get('/:siteId', function() {
     // TODO
     // read from db
