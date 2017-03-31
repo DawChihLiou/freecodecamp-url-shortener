a9a30aa0348978559cad342cad917c156afed3f5
Dawchih Liou <dawochih.liou@gmail.com>
2017-03-31 10:57:18 +0000

    add spinner

--- dist/app.js
+++ dist/app.js
@@ -4,6 +4,7 @@ $(function() {
     
     var $form = $('form');
     var $output = $('.output');
+    var $spinner = $('.spin');
     
     $form.on('submit', function(e) {
         e.preventDefault();
@@ -11,14 +12,21 @@ $(function() {
         
         var originalUrl = $(this).find('input').val();
         
+        $output.addClass('hidden');
+        $spinner.removeClass('hidden');
+        
         $.ajax({url: '/api/short/' + originalUrl})
             .done(function(data) {
-                var link = '<a href="' + data.short_url + '">' + data.short_url + '</a>'
+                var link = '<a href="' + data.short_url + '">' + data.short_url + '</a>';
                 $output.html(link);
             })
             .fail(function(err) {
                console.error(err);
                $output.text(err.message)
+            })
+            .always(function() {
+                $output.removeClass('hidden');
+                $spinner.addClass('hidden');
             });
     });
 });
